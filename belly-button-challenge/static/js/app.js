bebc30144d62224febea5095a6a4ff73675ab491
wallago-caite <caite.greenwood@gmail.com>
2024-08-18 18:01:53 -0500

    pushing html and js updates

--- belly-button-challenge/static/js/app.js
+++ belly-button-challenge/static/js/app.js
@@ -1,4 +1,7 @@
 // Function to build the metadata panel
+// uses d3.json() t o fetch data from sample file, extracts metadata fild 
+// selects the HTML element where the metadata is clears the content in the panle. 
+// appends new data, and the webpage gets updated with the info for the sample selected
 function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
     // Get the metadata field
@@ -11,32 +14,39 @@ function buildMetadata(sample) {
     // Use d3 to select the panel with id of `#sample-metadata`
     var PANEL = d3.select("#sample-metadata");
 
-    // Use `.html("")` to clear any existing metadata
+    // Clear any existing metadata
     PANEL.html("");
 
-    // Inside a loop, use d3 to append new tags for each key-value in the filtered metadata
+    // Append new tags for each key-value in the filtered metadata
     Object.entries(result).forEach(([key, value]) => {
-      PANEL.append("div").attr("class", "metadata-item").text(`${key.toUpperCase()}: ${value}`);});
+      PANEL.append("div").attr("class", "metadata-item").text(`${key.toUpperCase()}: ${value}`);
+    });
   });
 }
 
-// function to build both charts
+// Function to build both charts
+// on input sample, output is a bubble and bar chart.  
+// uses plotly, a JS-native, python-native plotting system
 function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
-
-    // Get the samples field
     var samples = data.samples;
-    
-    // Filter the samples for the object with the desired sample number
     var resultArray = samples.filter(x => x.id == sample);
     var result = resultArray[0];
 
-    // Get the otu_ids, otu_labels, and sample_values
     var otu_ids = result.otu_ids;
     var otu_labels = result.otu_labels;
     var sample_values = result.sample_values;
 
-    // Build a Bubble Chart
+    // Define consistent layout options
+    var layoutOptions = {
+      paper_bgcolor: '#2c3e50',  // Slate theme background
+      plot_bgcolor: '#34495e',   // Slightly different plot area color
+      font: { color: '#ecf0f1' }, // Light text for dark background
+      hoverlabel: { bgcolor: '#34495e', font: { color: '#ecf0f1' } }, // Consistent hover label styling
+      margin: { t: 30, l: 150 }
+    };
+
+    // Bubble Chart
     var bubbleTrace = [{
       x: otu_ids,
       y: sample_values,
@@ -45,27 +55,20 @@ function buildCharts(sample) {
       marker: {
         size: sample_values,
         color: otu_ids,
-        colorscale: 'Portland'
+        colorscale: 'Autumn'
       }
     }];
-
-    // Render the Bubble Chart
-    var bubbleLayout = {
-      title: 'Bacteria Cultures Per Sample',
+    var bubbleLayout = Object.assign({
+      title: 'Bacteria Per Sample',
       showlegend: false,
       height: 600,
       width: 1200,
       xaxis: { title: 'OTU ID' },
-      hovermode: 'closest',
-       colorscale: 'Portland'
-    };
+      hovermode: 'closest'
+    }, layoutOptions);
     Plotly.newPlot("bubble", bubbleTrace, bubbleLayout);
-    // For the Bar Chart, map the otu_ids to a list of strings for your yticks
 
-
-    // Build a Bar Chart
-    // Don't forget to slice and reverse the input data appropriately
-
+    // Bar Chart
     var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
     var barTrace = [{
       y: yticks,
@@ -74,50 +77,41 @@ function buildCharts(sample) {
       type: "bar",
       orientation: "h",
       marker: {
-        color: 'blue'
+        color: 'red'
       }
     }];
-    var barLayout = {
-      title: "Top 10 Bacteria Cultures Found",
-      margin: { t: 30, l: 150 },
-      paper_bgcolor: 'black',  
-      plot_bgcolor: 'black'    
-    };
-
-        // Render the Bar Chart
+    var barLayout = Object.assign({
+      title: "Top 10 Bacteria Cultures Found"
+    }, layoutOptions);
     Plotly.newPlot("bar", barTrace, barLayout);
   });
 }
 
 // Function to run on page load
+// suggestion from KK, SN in project-- have a default-loaded function
 function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
-
     // Get the names field
-    var Field_names = data.names;
+    var fieldNames = data.names;
 
-    // Use d3 to select the dropdown with id of `#selDataset`
+    // Select the dropdown menu
     var dropdownMenu = d3.select("#selDataset");
 
-    // Use the list of sample names to populate the select options
-    // Hint: Inside a loop, you will need to use d3 to append a new
-    // option for each sample name.
-    Field_names.forEach((sample) => {
-      dropdownMenu.append("option").text(sample).property("value", sample);});
+    // Populate the dropdown options
+    fieldNames.forEach((sample) => {
+      dropdownMenu.append("option").text(sample).property("value", sample);
+    });
 
-    // Get the first sample from the list
-    var firstSample = Field_names[0];
-
-    // Build charts and metadata panel with the first sample
+    // Build initial charts and metadata panel with the first sample
+    var firstSample = fieldNames[0];
     buildCharts(firstSample);
     buildMetadata(firstSample);
   });
 }
 
 // Function for event listener
-
 function optionChanged(newSample) {
-  // Build charts and metadata panel each time a new sample is selected
+  // Build charts and metadata panel for the selected sample
   buildCharts(newSample);
   buildMetadata(newSample);
 }
@@ -125,4 +119,4 @@ function optionChanged(newSample) {
 // Initialize the dashboard
 init();
 
-//References for this project: Jeremiah Mergenthaler, Saad Nasir
+//Resources Kevin Khan, Kourt Bailey(TA), ChatGPT 4.0
